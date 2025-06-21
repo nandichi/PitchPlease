f95a4429b97cfc81971628b02d642456ee976fb2
Andichi, Naoufal - Student Rijn IJssel <00313992@rijnijssel.nl>
2025-06-21 10:32:52 +0200

    Initial Commit

---
+++ PitchPlease/PitchPleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPleaseApp.swift
+//  PitchPlease
+//
+//  Created by Naoufal Andichi on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PitchPleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PitchPleaseTests/PitchPleaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPleaseTests.swift
+//  PitchPleaseTests
+//
+//  Created by Naoufal Andichi on 21/06/2025.
+//
+
+import Testing
+@testable import PitchPlease
+
+struct PitchPleaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
